c5e7c303c3118e33f063c29156479bdc70ae2f54
Julien Reboul <jreboul@salesforce.com>
2021-03-02 23:44:41 +0100

    feat(app): use a data file to manage the greetings

--- rest-app/src/main/java/com/example/springboot/HelloController.java
+++ rest-app/src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,23 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
 
 	@RequestMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
+	public String index(@RequestParam String greet) throws IOException {
+		Path fileName = Path.of("data.txt");
+		String actual = Files.readString(fileName);
+		String updatedGreets = actual + "<br>\n" + greet;
+		Files.writeString(fileName, updatedGreets);
+        return "Greetings to <br>" + updatedGreets;
 	}
-
 }
